06fbbca78a9cba5aeccdf943fa6e11a5e1fdcaeb
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-01-08 23:30:00 +0000

    license header; ws; unnecessary else

--- Morph/src/core/net/sf/morph/transform/converters/TextToTimeConverter.java
+++ Morph/src/core/net/sf/morph/transform/converters/TextToTimeConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package net.sf.morph.transform.converters;
 
 import java.text.DateFormat;
@@ -11,25 +26,23 @@ import net.sf.morph.transform.DecoratedConverter;
 import net.sf.morph.transform.transformers.BaseTransformer;
 
 public class TextToTimeConverter extends BaseTransformer implements Converter, DecoratedConverter {
-	
+
 	private static final DateFormat DEFAULT_DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
-	
+
 	private DateFormat dateFormat;
 	private Converter timeConverter;
 	private Converter textConverter;
 
 	protected Object convertImpl(Class destinationClass, Object source,
 			Locale locale) throws Exception {
-		
+
 		String text = (String) getTextConverter().convert(String.class, source, locale);
-		
+
 		if (ObjectUtils.isEmpty(text)) {
 			return null;
 		}
-		else {
-			Date date = getDateFormat().parse(text);			
-			return getTimeConverter().convert(destinationClass, date, locale);
-		}
+		Date date = getDateFormat().parse(text);
+		return getTimeConverter().convert(destinationClass, date, locale);
 	}
 
 	protected Class[] getSourceClassesImpl() throws Exception {
