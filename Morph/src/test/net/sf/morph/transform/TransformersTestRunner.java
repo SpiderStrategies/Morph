5e28089409cf8d8ba1917fd0bd635d18e8f95ab1
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-01-15 23:54:58 +0000

    add assemblercopier tests to suite

--- Morph/src/test/net/sf/morph/transform/TransformersTestRunner.java
+++ Morph/src/test/net/sf/morph/transform/TransformersTestRunner.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -31,6 +31,8 @@ import net.sf.morph.transform.converters.TextToTimeConverterTestCase;
 import net.sf.morph.transform.converters.TimeConverterTestCase;
 import net.sf.morph.transform.converters.TimeToNumberConverterTestCase;
 import net.sf.morph.transform.copiers.ArrayCopierTestCase;
+import net.sf.morph.transform.copiers.AssemblerCopierComponentsTestCase;
+import net.sf.morph.transform.copiers.AssemblerCopierTestCase;
 import net.sf.morph.transform.copiers.ContainerCopierTestCase;
 import net.sf.morph.transform.copiers.DelegatingCopierTestCase;
 import net.sf.morph.transform.copiers.PropertyNameMatchingCopierTestCase;
@@ -39,7 +41,7 @@ import net.sf.morph.transform.transformers.TypeChangingGraphTransformerTestCase;
 
 /**
  * Run tests for all converters
- * 
+ *
  * @author Matt Sgarlata
  * @since Nov 7, 2004
  */
@@ -48,20 +50,22 @@ public class TransformersTestRunner extends TestRunner {
 		TestSuite suite = new TestSuite();
 
 		// COPIERS
-		
+
 		suite.addTestSuite(ContainerCopierTestCase.class);
 		suite.addTestSuite(ArrayCopierTestCase.class);
 		suite.addTestSuite(PropertyNameMatchingCopierTestCase.class);
-		
+		suite.addTestSuite(AssemblerCopierTestCase.class);
+		suite.addTestSuite(AssemblerCopierComponentsTestCase.class);
+
 		// CONVERTERS
-		
+
 		suite.addTest(NumberConvertersTestRunner.suite());
 		suite.addTest(ToBooleanConvertersTestRunner.suite());
 		suite.addTest(ToTextConvertersTestRunner.suite());
-		
+
 		suite.addTestSuite(ObjectToClassConverterTestCase.class);
 		suite.addTestSuite(NumberConverterTestCase.class);
-		suite.addTestSuite(TextToTimeConverterTestCase.class);		
+		suite.addTestSuite(TextToTimeConverterTestCase.class);
 		suite.addTestSuite(TimeConverterTestCase.class);
 		suite.addTestSuite(IdentityConverterTestCase.class);
 		suite.addTestSuite(NumberToTimeConverterTestCase.class);
@@ -69,23 +73,23 @@ public class TransformersTestRunner extends TestRunner {
 		suite.addTestSuite(TextToNumberConverterTestCase.class);
 		suite.addTestSuite(TimeToNumberConverterTestCase.class);
 		suite.addTestSuite(PrimitiveWrapperConverterTestCase.class);
-		
+
 		// COMPOSITE CONVERTERS
-		
+
 		suite.addTestSuite(ChainedConverterTestCase.class);
 		suite.addTestSuite(DelegatingCopierTestCase.class);
 		suite.addTestSuite(ExplicitChainedConverterTestCase.class);
-		
+
 		// MISC TESTS
-		
+
 		suite.addTestSuite(SourceToDifferentDestinationsTestCase.class);
 		suite.addTestSuite(CyclicTransformationTestCase.class);
-		
+
 		// TRANSFORMERS
-		
+
 		suite.addTestSuite(SimpleDelegatingTransformerTestCase.class);
 		suite.addTestSuite(TypeChangingGraphTransformerTestCase.class);
-		
+
 		return suite;
 	}
 }
