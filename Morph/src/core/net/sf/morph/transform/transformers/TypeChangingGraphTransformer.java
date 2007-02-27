01c9cf2a0053dd853b468eb3cf28634d0659c56c
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-02-27 20:57:54 +0000

    ws

--- Morph/src/core/net/sf/morph/transform/transformers/TypeChangingGraphTransformer.java
+++ Morph/src/core/net/sf/morph/transform/transformers/TypeChangingGraphTransformer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -41,19 +41,19 @@ import net.sf.morph.util.TypeMap;
  * transform a domain model into a graph of transfer objects suitable for
  * transferring to a remote client or another tier of an n-tier architecture.
  * </p>
- * 
+ *
  * <p>
  * Note: if you are using this transformer to transformer an object graph
  * composed of CGLIB proxies (such as an object graph created by Hibernate), you
  * may need to specify your source types as interfaces, because CGLIB proxies
  * may not necessarily be instances of the expected class.
  * </p>
- * 
+ *
  * @author Matt Sgarlata
  * @since December 1, 2005
  */
 public class TypeChangingGraphTransformer extends SimpleDelegatingTransformer {
-	
+
 	private Map sourceToDestinationTypeMapping;
 
 	/**
@@ -64,35 +64,34 @@ public class TypeChangingGraphTransformer extends SimpleDelegatingTransformer {
 
 	protected Transformer getTransformer(Class transformerType) {
 		Object[] components = getComponents();
-		for (int i=0; i<components.length; i++) {
+		for (int i = 0; i < components.length; i++) {
 			if (transformerType.isAssignableFrom(components[i].getClass())) {
 				return (Transformer) components[i];
 			}
 		}
-		
 		throw new TransformationException(
 				"Could not find a component of type '"
 						+ transformerType.getName() + "' in transformer "
 						+ this);
 	}
-	
+
 	protected ContainerCopier getContainerCopier() {
 		return (ContainerCopier) getTransformer(ContainerCopier.class);
 	}
-	
+
 	protected PropertyNameMatchingCopier getPropertyNameMatchingCopier() {
 		return (PropertyNameMatchingCopier) getTransformer(PropertyNameMatchingCopier.class);
 	}
-	
+
 	protected IdentityConverter getIdentityConverter() {
 		return (IdentityConverter) getTransformer(IdentityConverter.class);
 	}
-	
+
 	protected Reflector createReflector() {
 		// create a reflector that will instantiate classes of the destination
 		// types designated by sourceToDestinationTypeMapping when it encounters
 		// objects that are instances of the source types designated in
-		// sourceToDestinationTypeMapping 
+		// sourceToDestinationTypeMapping
 		SimpleInstantiatingReflector instantiatingReflector = new SimpleInstantiatingReflector();
 		instantiatingReflector.setRequestedToInstantiatedTypeMap(sourceToDestinationTypeMapping);
 		// create a delegating reflector that will first do the instantiations
@@ -100,30 +99,30 @@ public class TypeChangingGraphTransformer extends SimpleDelegatingTransformer {
 		// reflection operations like reading property names
 		SimpleDelegatingReflector reflector = new SimpleDelegatingReflector();
 		List components = new ArrayList(Arrays.asList(reflector.getComponents()));
-		components.add(0, instantiatingReflector);		
+		components.add(0, instantiatingReflector);
 		reflector.setComponents(components.toArray(new Reflector[components.size()]));
 		return reflector;
 	}
-	
+
 	protected void initializeImpl() throws Exception {
 		super.initializeImpl();
-		
+
 		Reflector reflector = createReflector();
-		
+
 		ContainerCopier cc = getContainerCopier();
 		cc.setContainedSourceToDestinationTypeMap(getSourceToDestinationTypeMapping());
 		cc.setReflector(reflector);
-		
+
 		PropertyNameMatchingCopier pnmc = getPropertyNameMatchingCopier();
 		pnmc.setReflector(reflector);
-		
+
 		IdentityConverter identityConverter = getIdentityConverter();
 		identityConverter.setSourceClasses(ClassUtils.getImmutableTypes());
 		identityConverter.setDestinationClasses(ClassUtils.getImmutableTypes());
 
 		this.setReflector(reflector);
 	}
-	
+
 	protected Object convertImpl(Class destinationType, Object source, Locale locale) throws Exception {
 		Class transformedDestinationType =
 			TransformerUtils.getMappedDestinationType(
@@ -132,7 +131,7 @@ public class TypeChangingGraphTransformer extends SimpleDelegatingTransformer {
 			transformedDestinationType = destinationType;
 		}
 		return super.convertImpl(transformedDestinationType, source, locale);
-	}	
+	}
 
 	public Map getSourceToDestinationTypeMapping() {
 		return sourceToDestinationTypeMapping;
