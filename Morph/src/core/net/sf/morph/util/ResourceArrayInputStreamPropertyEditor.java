f3d2be5feeb6608ffd2544a36055b160045094d8
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-04-18 19:14:06 +0000

    license notices; javadoc

--- Morph/src/core/net/sf/morph/util/ResourceArrayInputStreamPropertyEditor.java
+++ Morph/src/core/net/sf/morph/util/ResourceArrayInputStreamPropertyEditor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package net.sf.morph.util;
 
 import java.beans.PropertyEditorSupport;
@@ -9,8 +24,9 @@ import org.springframework.core.io.support.ResourceArrayPropertyEditor;
 import org.springframework.util.StringUtils;
 
 /**
- * Drop-in replacement for Spring's InputStreamPropertyEditor, to
- * concatenate potentially multiple Resources into a single InputStream.
+ * Drop-in replacement for Spring's InputStreamPropertyEditor
+ * to convert one or more Resources to an InputStream.
+ * @author Matt Benson
  */
 public class ResourceArrayInputStreamPropertyEditor extends PropertyEditorSupport {
 
