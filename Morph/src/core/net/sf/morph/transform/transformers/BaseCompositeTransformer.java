d7865f05de0e8a4b8d1e0cb012877285ff75a870
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-04-24 22:29:38 +0000

    javadoc; automatic component array narrowing

--- Morph/src/core/net/sf/morph/transform/transformers/BaseCompositeTransformer.java
+++ Morph/src/core/net/sf/morph/transform/transformers/BaseCompositeTransformer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005, 2007 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -21,6 +21,7 @@ import net.sf.composite.StrictlyTypedComposite;
 import net.sf.composite.validate.ComponentValidator;
 import net.sf.morph.transform.NodeCopier;
 import net.sf.morph.transform.Transformer;
+import net.sf.morph.util.ClassUtils;
 import net.sf.morph.util.ContainerUtils;
 
 /**
@@ -29,32 +30,66 @@ import net.sf.morph.util.ContainerUtils;
  */
 public abstract class BaseCompositeTransformer extends BaseTransformer implements Transformer, SimpleComposite, StrictlyTypedComposite {
 
+	/** Our component list */
 	protected Object[] components;
 	private ComponentValidator componentValidator;
-	
+
 // internal state validation
-	
+
 	// TODO consider doing validation when data is specified rather than waiting
 	// until the user tries to perform an operation
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseTransformer#initializeImpl()
+	 */
 	protected void initializeImpl() throws Exception {
 		super.initializeImpl();
 		getComponentValidator().validate(this);
+		if (isNarrowingComponentArray()) {
+			if (components != null
+					&& !getComponentType().isAssignableFrom(
+							components.getClass().getComponentType())) {
+				Object[] newComponents = (Object[]) ClassUtils.createArray(
+						getComponentType(), components.length);
+				System.arraycopy(components, 0, newComponents, 0, components.length);
+				components = newComponents;
+			}
+		}
 		updateNestedTransformerComponents(getNestedTransformer(), null);
 	}
 
-// information about the components of a composite transformer 	
-	
+// information about the components of a composite transformer
+
+	/**
+	 * Return the component type of this transformer.
+	 * @return Class
+	 */
 	public Class getComponentType() {
 		return Transformer.class;
 	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.composite.SimpleComposite#getComponents()
+	 */
 	public Object[] getComponents() {
 		return components;
 	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.composite.SimpleComposite#setComponents(java.lang.Object[])
+	 */
 	public void setComponents(Object[] components) {
 		setInitialized(false);
 		this.components = components;
 	}
-	
+
+	/**
+	 * Return the ComponentValidator of this transformer.
+	 * @return ComponentValidator
+	 */
 	public ComponentValidator getComponentValidator() {
 		if (componentValidator == null) {
 			componentValidator = Defaults.createComponentValidator();
@@ -62,6 +97,10 @@ public abstract class BaseCompositeTransformer extends BaseTransformer implement
 		return componentValidator;
 	}
 
+	/**
+	 * Set the ComponentValidator for this transformer.
+	 * @param componentValidator ComponentValidator
+	 */
 	public void setComponentValidator(ComponentValidator componentValidator) {
 		this.componentValidator = componentValidator;
 	}
@@ -75,6 +114,11 @@ public abstract class BaseCompositeTransformer extends BaseTransformer implement
 		updateNestedTransformerComponents(nestedTransformer, old);
 	}
 
+	/**
+	 * Propagate our nested transformer to any component NodeCopiers.
+	 * @param incoming
+	 * @param outgoing
+	 */
 	protected void updateNestedTransformerComponents(Transformer incoming, Transformer outgoing) {
 		if (incoming == outgoing) {
 			return;
@@ -87,6 +131,10 @@ public abstract class BaseCompositeTransformer extends BaseTransformer implement
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseTransformer#isWrappingRuntimeExceptions()
+	 */
 	protected boolean isWrappingRuntimeExceptions() {
 		// composite transformers will often have user specified components,
 		// in which case we want to allow user user defined RuntimeExceptions
@@ -96,4 +144,12 @@ public abstract class BaseCompositeTransformer extends BaseTransformer implement
 	    return false;
     }
 
+	/**
+	 * Learn whether we should automatically narrow the array type of <code>components</code>
+	 * to that returned by {@link #getComponentType()} after validating the components.
+	 * @return default <code>true</code>
+	 */
+	protected boolean isNarrowingComponentArray() {
+		return true;
+	}
 }
