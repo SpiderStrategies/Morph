fa15fa5816f9eb1383c676f0e1fa95635b006513
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-04-24 22:35:30 +0000

    reworking; javadoc; use TUtil class intersection; use automatic component array narrowing

--- Morph/src/core/net/sf/morph/transform/copiers/MultipleDestinationConverter.java
+++ Morph/src/core/net/sf/morph/transform/copiers/MultipleDestinationConverter.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2004-2005 the original author or authors.
- * 
+ * Copyright 2004-2005, 2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -24,69 +24,101 @@ import net.sf.morph.transform.Converter;
 import net.sf.morph.transform.DecoratedConverter;
 import net.sf.morph.transform.Transformer;
 import net.sf.morph.transform.transformers.BaseCompositeTransformer;
+import net.sf.morph.util.TransformerUtils;
 
 /**
- * A converter which takes a single object and converts it into multiple
- * objects.
- * 
+ * A converter which takes a single object and converts it into multiple objects.
+ * TODO rework to a copier and rename DisassemblerCopier to complement AssemblerCopier.
+ *
  * @author Matt Sgarlata
  * @since Apr 18, 2005
  */
 public class MultipleDestinationConverter extends BaseCompositeTransformer implements DecoratedConverter {
-	
+
 	private Converter containerConverter;
 	private Class[] destinationClassesForEachDestination;
 
-	protected Object convertImpl(Class destinationClass, Object source,
-		Locale locale) throws Exception {
-
-		// 
-		Object[] components = getComponents();
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseTransformer#convertImpl(java.lang.Class, java.lang.Object, java.util.Locale)
+	 */
+	protected Object convertImpl(Class destinationClass, Object source, Locale locale)
+			throws Exception {
+		Converter[] converters = (Converter[]) getComponents();
 		Class[] destTypes = getDestinationClassesForEachDestination();
-		List destinationObjects = new ArrayList(components.length);
-		for (int i=0; i<components.length; i++) {
-			Converter converter = (Converter) components[i];
-			//Class destType = destTypes[i] == null ? Object.class : destTypes[i];
-			Object converted = converter.convert(destTypes[i], source, locale);
+		List destinationObjects = new ArrayList(converters.length);
+		for (int i = 0; i < components.length; i++) {
+			Object converted = converters[i].convert(destTypes[i], source, locale);
 			destinationObjects.add(converted);
 		}
-
 		return getContainerConverter().convert(destinationClass, destinationObjects, locale);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseCompositeTransformer#getComponentType()
+	 */
 	public Class getComponentType() {
-		return Transformer.class;
-	}	
-	
-	protected Class[] getSourceClassesImpl() throws Exception {
-		// FIXME should return those classes which can be accepted by all
-		// transformers
-		
-		return new Class[] { Object.class };
+		return Converter.class;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseTransformer#getSourceClassesImpl()
+	 */
+	protected Class[] getSourceClassesImpl() throws Exception {
+		return TransformerUtils.getSourceClassIntersection((Transformer[]) getComponents());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseTransformer#getDestinationClassesImpl()
+	 */
 	protected Class[] getDestinationClassesImpl() throws Exception {
 		return getContainerConverter().getDestinationClasses();
 	}
 
+	/**
+	 * Get the container converter that will be used by this converter.
+	 * @return Converter
+	 */
 	public Converter getContainerConverter() {
 		if (containerConverter == null) {
 			containerConverter = Defaults.createContainerCopier();
 		}
 		return containerConverter;
 	}
+
+	/**
+	 * Set the container converter that will be used by this converter.
+	 * @param containerTransformer Converter
+	 */
 	public void setContainerConverter(Converter containerTransformer) {
 		this.containerConverter = containerTransformer;
 	}
+
+	/**
+	 * Get the ordered array of destination classes.
+	 * @return Class[]
+	 */
 	public Class[] getDestinationClassesForEachDestination() {
 		return destinationClassesForEachDestination;
 	}
+
+	/**
+	 * Set an ordered array of destination classes.
+	 * @param destinations
+	 */
 	public void setDestinationClassesForEachDestination(Class[] destinations) {
 		this.destinationClassesForEachDestination = destinations;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.morph.transform.transformers.BaseCompositeTransformer#isWrappingRuntimeExceptions()
+	 */
 	protected boolean isWrappingRuntimeExceptions() {
 	    return false;
     }
-	
+
 }
