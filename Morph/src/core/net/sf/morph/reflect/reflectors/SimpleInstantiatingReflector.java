5ab78bba0f50d750739ea5f873df3668cac85e30
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-04-25 17:09:28 +0000

    cright; ws

--- Morph/src/core/net/sf/morph/reflect/reflectors/SimpleInstantiatingReflector.java
+++ Morph/src/core/net/sf/morph/reflect/reflectors/SimpleInstantiatingReflector.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2004-2005, 2007 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -32,14 +32,14 @@ import net.sf.morph.util.TypeMap;
  * class types to be mapped to instantiated types.  The default instance of this
  * class can instantiate common interfaces such as {@link java.util.Calendar}
  * and {@link java.lang.CharSequence}.
- * 
+ *
  * @author Matt Sgarlata
  * @since Feb 27, 2005
  */
 public class SimpleInstantiatingReflector extends BaseReflector implements InstantiatingReflector {
-	
+
 	private Map requestedToInstantiatedTypeMap;
-	
+
 	public SimpleInstantiatingReflector() {
 		super();
 	}
@@ -53,18 +53,18 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 		setRequestedType(requestedType);
 		setInstantiatedType(instantiatedType);
 	}
-	
+
 	public SimpleInstantiatingReflector(Map requestedToInstantiatedClassMap) {
 		super();
 		this.requestedToInstantiatedTypeMap = requestedToInstantiatedClassMap;
 	}
-	
+
 	protected Class[] getReflectableClassesImpl() throws Exception {
 		Set reflectableClasses = getRequestedToInstantiatedTypeMap().keySet();
 		return (Class[]) reflectableClasses.toArray(new Class[reflectableClasses.size()]);
 	}
-	
-	protected Object newInstanceImpl(Class requestedType, Object parameters) throws Exception {	
+
+	protected Object newInstanceImpl(Class requestedType, Object parameters) throws Exception {
 		Class typeToInstantiate = TransformerUtils.getMappedDestinationType(
 				getRequestedToInstantiatedTypeMap(), requestedType);
 		if (typeToInstantiate == null) {
@@ -76,16 +76,16 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 	/**
 	 * Returns the instantiated type if only a single mapping of requested type
 	 * to instantiated type has been specified.
-	 * 
+	 *
 	 * @return the instantiated type, if one has been specified or <br>
 	 *         <code>null</code> if no instantiated type has been specified
 	 * @throws IllegalStateException
 	 *             if there is more than one mapping of requested type to
 	 *             instantiated type
 	 */
-	public final Class getInstantiatedType() {		
+	public final Class getInstantiatedType() {
 		if (ObjectUtils.isEmpty(getRequestedToInstantiatedTypeMap())) {
-			return null; 
+			return null;
 		}
 		if (getRequestedToInstantiatedTypeMap().size() == 1) {
 			return (Class) getRequestedToInstantiatedTypeMap().values().iterator().next();
@@ -95,11 +95,11 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 			+ "single instantiated type cannot be returned.  The mappings "
 			+ "are " + getRequestedToInstantiatedTypeMap());
 	}
-	
+
 	/**
 	 * Sets the instantiated type if only a single mapping of requested type to
 	 * instantiated type is needed.
-	 * 
+	 *
 	 * @param instantiatedType
 	 *            the instantiated type
 	 */
@@ -107,7 +107,7 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 		if (getRequestedToInstantiatedTypeMap() == null) {
 			setRequestedToInstantiatedTypeMap(new TypeMap());
 		}
-		
+
 		Class requestedType;
 		try {
 			requestedType = getRequestedType();
@@ -115,15 +115,15 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 		catch (IllegalStateException e) {
 			requestedType = null;
 		}
-		
+
 		getRequestedToInstantiatedTypeMap().clear();
 		getRequestedToInstantiatedTypeMap().put(requestedType, instantiatedType);
 	}
-	
+
 	/**
 	 * Returns the requested type if only a single mapping of requested type to
 	 * instantiated type has been specified.
-	 * 
+	 *
 	 * @return the requested type, if one has been specified or <br>
 	 *         <code>null</code> if no requested type has been specified
 	 * @throws IllegalStateException
@@ -132,7 +132,7 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 	 */
 	public final Class getRequestedType() {
 		if (ObjectUtils.isEmpty(getRequestedToInstantiatedTypeMap())) {
-			return null; 
+			return null;
 		}
 		if (getRequestedToInstantiatedTypeMap().size() == 1) {
 			return (Class) getRequestedToInstantiatedTypeMap().keySet().iterator().next();
@@ -142,11 +142,11 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 			+ "single requested type cannot be returned.  The mappings "
 			+ "are " + getRequestedToInstantiatedTypeMap());
 	}
-	
+
 	/**
 	 * Sets the requested type if only a single mapping of requested type to
 	 * instantiated type is needed.
-	 * 
+	 *
 	 * @param requestedType
 	 *            the requested type
 	 */
@@ -154,7 +154,7 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 		if (getRequestedToInstantiatedTypeMap() == null) {
 			setRequestedToInstantiatedTypeMap(new TypeMap());
 		}
-		
+
 		Class instantiatedType;
 		try {
 			instantiatedType = getInstantiatedType();
@@ -162,21 +162,21 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 		catch (IllegalStateException e) {
 			instantiatedType = null;
 		}
-		
+
 		getRequestedToInstantiatedTypeMap().clear();
 		getRequestedToInstantiatedTypeMap().put(requestedType, instantiatedType);
 	}
-	
+
 	/**
 	 * Returns the mapping of requested types to instantiated types.
-	 * 
+	 *
 	 * @return the mapping of requested types to instantiated types
 	 */
 	public synchronized Map getRequestedToInstantiatedTypeMap() {
 		if (requestedToInstantiatedTypeMap == null) {
 			Map map = new TypeMap();
 			map.put(Calendar.class, GregorianCalendar.class);
-			
+
 			if (ClassUtils.isJdk14OrHigherPresent()) {
 				try {
 					map.put(Class.forName("java.lang.CharSequence"), StringBuffer.class);
@@ -194,7 +194,7 @@ public class SimpleInstantiatingReflector extends BaseReflector implements Insta
 
 	/**
 	 * Sets the mapping of requested types to instantiated types.
-	 * 
+	 *
 	 * @param interfaceToImplementationMap
 	 *            the mapping of requested types to instantiated types
 	 */
