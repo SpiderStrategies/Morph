9056713b533ae7d1dedf401a7a3f4030b754596d
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-12-17 21:45:24 +0000

    velocity integration refactoring

--- Morph/src/core/net/sf/morph/reflect/reflectors/VelocityContextReflector.java
+++ Morph/src/core/net/sf/morph/reflect/reflectors/VelocityContextReflector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2004-2005, 2007 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,61 +15,12 @@
  */
 package net.sf.morph.reflect.reflectors;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.context.Context;
-
 /**
  * Exposes the information in a {@link org.apache.velocity.context.Context}.
  * 
  * @author Matt Sgarlata
  * @since Dec 29, 2004
+ * @deprecated version 1.0.2 use net.sf.morph.integration.velocity
  */
-public class VelocityContextReflector extends BaseBeanReflector {
-	
-	private static final Class[] REFLECTABLE_TYPES = { VelocityContext.class };
-
-	protected Class[] getReflectableClassesImpl() throws Exception {
-		return REFLECTABLE_TYPES;
-	}
-
-	protected Context getVelocityContext(Object bean) {
-		return (Context) bean;
-	}
-	
-	protected String[] getPropertyNamesImpl(Object bean) throws Exception {
-		Object[] keys = getVelocityContext(bean).getKeys();
-		List propertyNames = new ArrayList();
-		for (int i=0; i<keys.length; i++) {
-			if (keys[i] instanceof String) {
-				propertyNames.add(keys[i]);
-			}
-		}
-		return (String[]) propertyNames.toArray(new String[propertyNames.size()]);
-	}
-
-	protected Class getTypeImpl(Object bean, String propertyName) throws Exception {
-		return Object.class;
-	}
-
-	protected boolean isReadableImpl(Object bean, String propertyName) throws Exception {
-		return true;
-	}
-
-	protected boolean isWriteableImpl(Object bean, String propertyName) throws Exception {
-		return true;
-	}
-
-	protected Object getImpl(Object bean, String propertyName) throws Exception {
-		return getVelocityContext(bean).get(propertyName);
-	}
-
-	protected void setImpl(Object bean, String propertyName, Object value) throws Exception {
-		getVelocityContext(bean).put(propertyName, value);
-	}
-	
-	
-
+public class VelocityContextReflector extends net.sf.morph.integration.velocity.VelocityContextReflector {
 }
