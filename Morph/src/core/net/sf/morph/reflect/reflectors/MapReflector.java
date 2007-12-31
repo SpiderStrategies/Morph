3872b63b967e8015932fd4e9ac1c213761bb0260
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2007-12-31 20:09:42 +0000

    unused import

--- Morph/src/core/net/sf/morph/reflect/reflectors/MapReflector.java
+++ Morph/src/core/net/sf/morph/reflect/reflectors/MapReflector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2004-2005, 2007 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -16,7 +16,6 @@
 package net.sf.morph.reflect.reflectors;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
