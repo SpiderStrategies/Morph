2df6fae2acabd665ba29e2d1ad7519861ee82ced
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2008-02-07 23:37:44 +0000

    ws

--- Morph/src/core/net/sf/morph/transform/transformers/BaseReflectorTransformer.java
+++ Morph/src/core/net/sf/morph/transform/transformers/BaseReflectorTransformer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004-2005 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -23,23 +23,23 @@ import net.sf.morph.reflect.MutableIndexedContainerReflector;
 
 /**
  * A base class for transformers that are implemented using a Reflector.
- * 
+ *
  * @author Matt Sgarlata
  * @since Dec 24, 2004
  */
 public class BaseReflectorTransformer extends BaseTransformer {
-	
+
 	public BaseReflectorTransformer() {
 		super();
 	}
-	
+
 	protected Class[] getDestinationClassesImpl() throws Exception {
 		return getReflector().getReflectableClasses();
 	}
 	protected Class[] getSourceClassesImpl() throws Exception {
 		return getReflector().getReflectableClasses();
 	}
-	
+
 	protected BeanReflector getBeanReflector() {
 		return (BeanReflector) getReflector(BeanReflector.class);
 	}
