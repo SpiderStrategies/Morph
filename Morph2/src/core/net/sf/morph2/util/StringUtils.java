1812157e266af7d1fd8866de0d6d03c9a755bb22
matt.benson <matt.benson@cc3055bb-40f1-4749-b579-52a34e25c62a>
2010-03-28 14:21:00 +0000

    remove copyright statements from individual files after having added the copyright to NOTICE.txt; this saves having to remember to update the copyright in each file, each year a change is made

--- Morph2/src/core/net/sf/morph2/util/StringUtils.java
+++ Morph2/src/core/net/sf/morph2/util/StringUtils.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2004-2005, 2010 the original author or authors.
- * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
@@ -12,7 +10,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package net.sf.morph2.util;
+ */
+package net.sf.morph2.util;
 
 import java.lang.reflect.Array;
 import java.util.Collection;
