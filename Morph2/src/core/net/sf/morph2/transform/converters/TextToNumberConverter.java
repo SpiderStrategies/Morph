75ee1a0aeebb20ba1c5bf9c8d8429385e57a373a
Matt Sgarlata <matt.sgarlata@spiderstrategies.com>
2015-01-20 15:11:43 -0500

    speed improvement (merge forward 3a8fc1be3658ec792d92439447be378a5758f507)

--- Morph2/src/core/net/sf/morph2/transform/converters/TextToNumberConverter.java
+++ Morph2/src/core/net/sf/morph2/transform/converters/TextToNumberConverter.java
@@ -301,6 +301,7 @@ public class TextToNumberConverter extends BaseTransformer implements DecoratedC
 	 */
 	private StringBuffer removeIgnoredCharacters(String string, Locale locale) {
 		StringBuffer charactersToParse = new StringBuffer();
+		DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
 	    for (int i = 0; i < string.length(); i++) {
 			char currentChar = string.charAt(i);
 			if (getWhitespaceHandling() == WHITESPACE_IGNORE
@@ -312,7 +313,6 @@ public class TextToNumberConverter extends BaseTransformer implements DecoratedC
 				continue;
 			}
 			if (getPercentageHandling() == PERCENTAGE_IGNORE) {
-				DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
 				if (currentChar == symbols.getPercent()) {
 					continue;
 				}
